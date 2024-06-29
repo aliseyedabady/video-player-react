dab0b773893f4ad248326a774c93a252046d739d
Ali Seyed Abady <66261775+aliseyedabady@users.noreply.github.com>
2024-06-29 11:32:09 +0330

    Add play and pause to video player

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,14 +1,63 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { VideoProps } from "./type";
 
 const Video: React.FC<VideoProps> = ({ src, width, height }) => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlayPause = () => {
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+        setIsPlaying(true);
+      } else {
+        videoRef.current.pause();
+        setIsPlaying(false);
+      }
+    }
+  };
 
   return (
-    <div>
-      <video width={width || 640} height={height || 360} ref={videoRef}>
+    <div
+      className="relative"
+      style={{ width: width || 640, height: height || 360 }}
+    >
+      <video
+        width={width || 640}
+        height={height || 360}
+        ref={videoRef}
+        className="relative"
+      >
         <source src={src} type="video/mp4" />
       </video>
+      <div className="absolute w-full left-0 top-0 h-full flex items-center justify-center">
+        <button onClick={handlePlayPause}>
+          {isPlaying ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" />
+              <path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" />
+            </svg>
+          )}
+        </button>
+      </div>
     </div>
   );
 };
